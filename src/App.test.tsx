cac53c965ecc490af4a7791b4893f219980254c9
renato76 <renmignogna@gmail.com>
2021-08-05 15:33:08 +0100

    setup initial files and simple test in react testing library

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders star wars heading', () => {
+  render(<App />)
+  const linkElement = screen.getByText('Star Wars')
+  expect(linkElement).toBeInTheDocument()
+})
